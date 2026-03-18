7323ccfa715fb17a0533d6ce8645903d93462cd9
Zaureau15 <56055012+Zaureau15@users.noreply.github.com>
2026-03-18 11:28:19 -0400

    Improve build_sphinx docs

--- docs/build_sphinx.bash
+++ docs/build_sphinx.bash
@@ -1,4 +1,7 @@
-# Run this from the root directory, not the docs directory
+# Run this from the root directory, not the docs directory.
+# This command also runs on Windows if you have bash installed.
+# It builds the docs, then opens them in your browser.
+# Commend out the "start" line to stop the browser from auto-opening.
 
 sphinx-build -b html docs docs/build/html
 start docs/build/html/index.html
